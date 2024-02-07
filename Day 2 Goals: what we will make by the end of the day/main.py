35153ed9ba162c6bd57b68333e9102843d32873e
Emmayordi <yordanosteklay424@gmail.com>
2024-02-07 12:06:40 +0100

    band name genereter

---
+++ Day 2 Goals: what we will make by the end of the day/main.py
@@ -0,0 +1,2 @@
+#Day 2 Goals: what we will make by the end of the day
+print("hello"[0])
---
+++ Day one Band ganerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band grnerater.")
+city = input("which city did you grow up in?")
+pet = input("what is the name of pet?")
+print("your band name could be " + city + " "+ pet)
